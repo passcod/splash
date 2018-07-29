11835612fe471acd7c5f67e32dd62a4fa92bcd3a
Félix Saparelli <felix@passcod.name>
2018-07-29 22:22:05 +1200

    Use geo and gdal crates

--- README.md
+++ README.md
@@ -1,8 +1,7 @@
 # Splash
 
 **[SPLAT!]** is a wonderful tool for RF analysis over terrain. Unfortunately,
-it only works with a few terrain sources, supports no more than four emitters
-in one analysis, and makes use of only a single core.
+it only works with a few terrain sources and only uses one core for processing.
 
 **Splash** is not a rewrite of SPLAT!. It is a different implementation of the
 _idea_ of SPLAT!, but taking different routes and making different decisions.
@@ -11,6 +10,6 @@ _idea_ of SPLAT!, but taking different routes and making different decisions.
 
  - Splash only understands metres, and internally, it uses SI units.
  - Splash calculates local geocentric radii instead of assuming spherical Earth.
- - Splash does not output pretty pictures, but raster GIS data.
- - Splash uses geodesic distance calculations ([Karney 2013](https://doi.org/10.1007/s00190-012-0578-z)).
-
+ - Splash uses Vicenty distance calculations rather than Haversine most times.
+ - Splash does not output pretty pictures, but raster GIS data (GeoTIFF).
+ - Splash does not use SDF input files, but raster GIS data (GeoTIFF).
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,10 @@
 #![forbid(unsafe_code)]
 #![cfg_attr(feature = "cargo-clippy", deny(clippy_pedantic))]
 
+extern crate gdal;
+extern crate geo;
+
+use geo::Point;
 use std::path::PathBuf;
 
 const EQUATORIAL_RADIUS: f64 = 6_378_137.0;
@@ -28,35 +32,10 @@ fn test_local_radius() {
     assert_eq!(local_radius(-1.469167), 6356974.249836446);
 }
 
-// in degrees
-struct Point {
-    pub lat: f64,
-    pub lon: f64,
-}
-
-/// Computes the distance and forward azimuth on the Earth between two points.
-///
-/// Uses “inverse” geodesic equations from [Karney 2013] rather than Haversine
-/// (which has large errors) or Valenty’s (which is unstable for some inputs).
-/// Assumes WGS84 geography.
-///
-/// While inverse geodesic equations can have multiple solutions, the distance
-/// remains the same for all results. At the moment, we ignore further
-/// results for the azimuth and return only the first found.
-///
-/// This function was written with great help from [geographic]’s Geodesy Java
-/// library, written by the author of the paper cited above.
-///
-/// [Karney 2013]: https://doi.org/10.1007/s00190-012-0578-z
-/// [geographic]: https://geographiclib.sourceforge.io/html/java/
-fn geodesic_distance(a: Point, b: Point) -> (f64, f64) {
-    (0.0, 0.0)
-}
-
-/// TX or RX site definition
+/// Base site definition.
 struct Site {
     /// Where it is
-    position: Point,
+    position: Point<f64>,
 
     /// How high above ground
     aboveground: f64,
@@ -65,8 +44,10 @@ struct Site {
     name: String,
 }
 
-/// Parameters for a Site
-struct Parameters {
+/// An RF transmitter and its parameters.
+struct Transmitter {
+    site: Site,
+
     /// Earth Dielectric Constant (Relative permittivity)
     dielectric: f64,
 
